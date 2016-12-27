acbead5ed96babc128ec21fa6739500b27c85540
Indri Muska <indrimuska@gmail.com>
2016-12-27 18:49:52 +0100

    Replacing Grunt with Webpack

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+let pkg = require('./package');
+let webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
+let extractTextPlugin = require('extract-text-webpack-plugin');
+
+let filename = 'angular-moment-picker' + (process.argv.filter(arg => arg == '-p').length > 0 ? '.min' : '');
+
+module.exports = {
+	entry: './src/index.ts',
+	output: {
+		path: './dist/',
+		filename: filename + '.js'
+	},
+	bail: true,
+	externals: Object.keys(pkg.dependencies),
+	resolve: {
+		extensions: ['', '.ts', '.html', '.less']
+	},
+	module: {
+		preLoaders: [
+			{ test: /\.ts$/, loader: 'tslint' }
+		],
+		loaders: [
+			{ test: /\.ts$/, loader: 'ts' },
+			{ test: /\.html$/, loader: 'html?minimize=true' },
+			{ test: /\.less$/, loader: extractTextPlugin.extract('style', 'css!postcss!less') }
+		]
+	},
+	plugins: [
+		new extractTextPlugin(filename + '.css'),
+		new webpack.BannerPlugin('Angular Moment Picker - v' + pkg.version + ' - ' + pkg.homepage + ' - (c) 2015 Indri Muska - ' + pkg.license)
+	],
+	postcss: [
+		autoprefixer
+	]
+};
