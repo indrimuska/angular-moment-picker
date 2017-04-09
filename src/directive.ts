ade450cf5b56479a4c267a50afc1468d62ebc282
Indri Muska <indrimuska@gmail.com>
2017-04-09 23:08:41 +0200

    Fix return fall on keydown
    Ref. #158

--- src/directive.ts
+++ src/directive.ts
@@ -409,7 +409,9 @@ export default class Directive implements ng.IDirective {
 			$scope.input
 				.on('focus click', () => $scope.$evalAsync($scope.view.open))
 				.on('blur',        () => $scope.$evalAsync($scope.view.close))
-				.on('keydown',     (e) => $scope.keyboard && $scope.$evalAsync(() => $scope.view.keydown(e)));
+				.on('keydown',     (e) => {
+					if ($scope.keyboard) $scope.$evalAsync(() => $scope.view.keydown(e))
+				});
 			$scope.contents.on('click', () => focusInput());
 			$scope.container.on('mousedown', (e: JQueryEventObject) => focusInput(e));
 			angular.element(this.$window).on('resize scroll', $scope.view.position);
